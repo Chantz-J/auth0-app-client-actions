ea3f547268708b6f991f2f8057a28d0fa3a8e006
Chantz-J <johnson.chantz@gmail.com>
2022-08-24 16:03:06 -0400

    fixed typo in server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express();
 const axios = require('axios').default;
 const request = require('request');
 require('dotenv').config();
+// const { auth } = require('express-openid-connect');
 // const { auth, requiresAuth } = require('express-openid-connect');
 
 // Authorization middleware. When used, the Access Token must 
@@ -13,11 +14,20 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const DOMAIN = process.env.DOMAIN;
 
-app.get('/', function(req, res) {
-    res.json({
-        message: 'Hello World!'
-    });
+
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 });
+
+// app.get('/', function(req, res) {
+//     res.json({
+//         message: 'Hello World!'
+//     });
+// });
 
 app.get('/list', function(req, res) {
 
@@ -83,7 +93,7 @@ app.get('/list', function(req, res) {
         //Look for client_id substring in string of action's code block
         for (let i = 0; i < clients[0].length; i++) {
             for (let j = 0; j < actions[0].length; j++) {
-              if (actions[0][j].code.includes(clients[0][i].name)) {
+              if (actions[0][j].code.includes(clients[0][i].client_id)) {
                 // Pair any matches, and log a string of these matches, printing the name of the action, 
                 // the client it references, 
                 // and the supported triggers.
